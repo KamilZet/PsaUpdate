59adeb7179c256a748e12246b925c49a4d17fe4e
KamilZet <kamilxzien@gmail.com>
2016-04-15 11:39:17 +0200

    Sources were added

---
+++ PsaUpdate/Deployer.cs
@@ -0,0 +1,109 @@
+﻿using PsaUpdate.Properties;
+using System;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace PsaUpdate
+{
+    class Deployer
+    {
+        private string oldFolderName = null;
+        private string newSubFolderName = null;
+        private string newFolderName = null;
+        private string oldFileName = null;
+        private string newFileName = null;
+
+        public Deployer()
+        {
+            oldFolderName = SharedComponentsDir();
+            newSubFolderName = "PSAIO_BAK";
+            newFolderName = Path.Combine(oldFolderName, newSubFolderName);
+            oldFileName = "PSAIO.dll";
+            newFileName = "PSAIO_v12.dll";
+        }
+
+        public string OldFolderName
+        {
+            get { return oldFolderName; }
+        }
+
+        public string Run()
+        {
+
+            if (!Helper.IsAdministrator()) return "No administrator privileges associated with the current profile!";
+
+            string oldFullFileName = Path.Combine(oldFolderName,oldFileName);
+            string newFullFileName = Path.Combine(newFolderName,newFileName);
+
+            string ProcessMessage = null;
+ 
+            DirectorySecurity ds = new DirectorySecurity();
+            ds.AddAccessRule(new FileSystemAccessRule(@"everyone",FileSystemRights.FullControl,AccessControlType.Allow));
+
+
+            try
+            {
+                if (Directory.Exists(newFolderName)) Directory.Delete(newFolderName,true);
+
+                DirectoryInfo destFolderName = Directory.CreateDirectory(newFolderName);
+
+                if (File.Exists(oldFullFileName)) File.Move(oldFullFileName, newFullFileName);
+                File.WriteAllBytes(oldFullFileName, Resources.PSAIO);
+
+                ProcessMessage = "success";
+
+            }
+            catch (Exception e)
+            {
+                ProcessMessage = "failure (" + e.Message + ")";
+            }
+
+            return ProcessMessage;
+
+        }
+
+        public bool IsValidSharedComponentsDir()
+        {
+            if (Directory.Exists(this.oldFolderName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string SharedComponentsDir()
+        {
+            if (IsX64Arch())
+                return @"C:\Program Files (x86)\ACNielsen\Shared Components";
+            else
+                return
+                    @"C:\Program Files\ACNielsen\Shared Components"; 
+        }
+        public static bool IsX64Arch()
+        {
+            return Environment.Is64BitOperatingSystem;
+
+            //if (string.Equals(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"),"AMD64",StringComparison.CurrentCultureIgnoreCase))
+            //    return true;
+            //else
+            //    return false;
+        }
+
+        public static string IsX64ArchToString()
+        {
+            if (IsX64Arch())
+                return "64 bit";
+            else
+                return "32 bit";
+
+            //if (string.Equals(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE"),"AMD64",StringComparison.CurrentCultureIgnoreCase))
+            //    return true;
+            //else
+            //    return false;
+        }
+
+    }
+}
---
+++ PsaUpdate/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsaUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string result = null;
+            Deployer d = new Deployer();
+
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("* This script will update your PSA handling mechanism");
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("Auxiliary information collected from your current PC / Spaceman configuration");
+            Console.WriteLine("Operating system: {0}",Deployer.IsX64ArchToString());
+            Console.WriteLine("Spaceman shared components root dir: {0} | Is valid: {1}", d.OldFolderName, d.IsValidSharedComponentsDir().ToString());
+            Console.WriteLine("****************************************************");
+            Console.WriteLine("* Press y to continue or n to break");
+            Console.WriteLine("****************************************************");
+            var input = Console.ReadKey(true);
+            switch (input.Key)
+            {
+                case ConsoleKey.Y:
+                    {
+                        
+                        result = d.Run();    
+                    }
+                    Console.WriteLine("* Update status: {0}", result.ToUpper());
+                    Console.WriteLine("****************************************************");
+                    break;
+                case ConsoleKey.N:
+                    Console.WriteLine("* Update processing cancelled");
+                    Console.WriteLine("****************************************************");
+                    break;
+                default:
+                    Console.WriteLine("* Invalid key pressed | Update processing cancelled");
+                    Console.WriteLine("****************************************************");
+                    break;
+            }
+            Console.WriteLine("* Hit any key to exit");
+            Console.WriteLine("****************************************************");
+            Console.ReadKey();
+
+        }
+
+    }
+}
